6aaea26d8eb8d05a278dbe73cf95c0332efd0ad6
Paul Saxe <paul@thesaxes.net>
2020-12-04 13:58:53 -0500

    Adding the simplest test: just create the object.

--- tests/test_crystal_builder_step.py
+++ tests/test_crystal_builder_step.py
@@ -5,3 +5,11 @@
 
 import pytest  # noqa: F401
 import crystal_builder_step  # noqa: F401
+
+
+def test_construction():
+    """Just create an object and test its type."""
+    result = crystal_builder_step.CrystalBuilder()
+    assert str(type(result)) == (
+        "<class 'crystal_builder_step.crystal_builder.CrystalBuilder'>"  # noqa: E501
+    )
